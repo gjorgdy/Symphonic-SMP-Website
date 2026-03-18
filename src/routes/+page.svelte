39579729bf0ddc7f1cb7881cd18b3ee2e26e867e
Gjorgdy <jordy.reins@proton.me>
2026-03-18 01:56:46 +0100

    ALT: changed players to symphonists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,26 +8,26 @@
     let page: string = $state("videos");
 </script>
 
-<div class="flex flex-col h-full">
-<div class="grid grid-cols-3 w-full h-10 p-2 mb-2 rounded-md md:rounded-xl bg-[#1e1e1e] divide-[#444444] divide-x-2 md:hidden">
+<div class="flex flex-col h-full w-full">
+<div class="flex flex-row h-10 p-2 mb-2 rounded-md md:rounded-xl bg-[#1e1e1e] divide-[#444444] divide-x-2 md:hidden">
     <button
             type="button"
-            class={"flex items-center justify-center " + (page === "videos" ? "underline" : "")}
+            class={"flex flex-1 items-center justify-center " + (page === "videos" ? "underline" : "")}
             onclick={(() => page = "videos")}
     >Videos</button>
     <button
         type="button"
-        class={"flex items-center justify-center " + (page === "players" ? "underline" : "")}
+        class={"flex flex-2 items-center justify-center " + (page === "players" ? "underline" : "")}
         onclick={(() => page = "players")}
-    > Players </button>
+    > Symphonists </button>
     <button
             type="button"
-            class={"flex items-center justify-center " + (page === "links" ? "underline" : "")}
+            class={"flex flex-1 items-center justify-center " + (page === "links" ? "underline" : "")}
             onclick={(() => page = "links")}
     > Links </button>
 </div>
 
-<div class="grid grid-cols-1 md:grid-cols-[1fr_2fr] grid-rows-1 md:grid-rows-[1fr_5fr] gap-4 max-w-[95dvw] w-full overflow-hidden">
+<div class="grid grid-cols-1 md:grid-cols-[1fr_2fr] grid-rows-1 md:grid-rows-[1fr_5fr] gap-4 overflow-hidden">
 
     <!--    About     -->
     <div class={"rounded-xl bg-[#1e1e1e] flex flex-col p-4 gap-4 " + (page === "links" ? "" : "not-md:hidden")}>
@@ -52,7 +52,7 @@
 
     <!--    Players     -->
     <div class={"md:row-start-2 rounded-xl bg-[#1e1e1e] min-h-0 overflow-hidden " + (page === "players" ? "" : "not-md:hidden")}>
-        <h2 class="text-xl pixel p-4 not-md:hidden">Players</h2>
+        <h2 class="text-xl pixel p-4 not-md:hidden">Symphonists</h2>
         <div class="flex flex-col gap-4 p-4 md:pt-0 h-full min-h-0 overflow-y-auto">
             {#each data.players as player}
                 <PlayerListItem {player}/>
