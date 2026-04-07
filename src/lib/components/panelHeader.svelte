ca7448b0200545b6e8016ec6346e84c1cab5abc3
Gjorgdy <jordy.reins@proton.me>
2026-04-07 16:53:31 +0200

    ADD: deselect disc by clicking name in content subtitle

--- src/lib/components/panelHeader.svelte
+++ src/lib/components/panelHeader.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {type Panel, setPanel} from "$lib/utils/navigationUtils";
+    import {type Panel, setDisc, setPanel} from "$lib/utils/navigationUtils";
     import {twMerge} from "tailwind-merge";
 
     type PanelHeaderProps = {
@@ -14,10 +14,20 @@
 
 <div class={twMerge("flex flex-col md:flex-row not-md:gap-4 pb-4 w-full justify-between", className)}>
     <div class="h-fit flex flex-row gap-4 text-xl pixel items-center">
-        <button class="flex flex-row items-end gap-2 overflow-hidden cursor-pointer" onclick={async () => await setPanel(panelName)}>
-            {title ?? ""}
-            <span class="text-gray-600 text-sm overflow-hidden text-nowrap text-ellipsis">{subtitle ?? ""}</span>
-        </button>
+        <div class="flex flex-row items-end gap-2 overflow-hidden">
+            <button
+                class="hover:text-gray-300 transition-colors cursor-pointer"
+                onclick={async () => await setPanel(panelName)}
+            >
+                {title ?? ""}
+            </button>
+            <button
+                class="text-gray-600 text-sm overflow-hidden text-nowrap text-ellipsis hover:text-gray-700 hover:line-through transition-colors cursor-pointer"
+                onclick={async () => await setDisc(undefined)}
+            >
+                {subtitle ?? ""}
+            </button>
+        </div>
     </div>
     {#if children}
         {@render children()}
