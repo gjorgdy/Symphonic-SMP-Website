f5cda85756735ba98c61ba45f28334c93ad617e5
Gjorgdy <jordy.reins@proton.me>
2026-04-07 16:45:52 +0200

    ALT: allow index and single panels on desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,46 @@
     import Content from "$lib/components/index/content.svelte";
     import Links from "$lib/components/index/links.svelte";
     import type {PlayerDisplay} from "$lib/models/player";
+    import { readable } from 'svelte/store';
+    import {twMerge} from "tailwind-merge";
 
     const { data } = $props();
+
+    const activePanel = $derived.by(() => data.panel ?? "content");
+
+    type ChangeEventHandler = (this:MediaQueryList, ev: MediaQueryListEvent) => any;
+
+    const isDesktop = readable(false, (set) => {
+        if (typeof window === 'undefined') return;
+        const query = window.matchMedia('(min-width: 768px)');
+        // Set the initial value
+        set(query.matches);
+        const handler: ChangeEventHandler = (e) => set(e.matches);
+        query.addEventListener('change', handler);
+        return () => query.removeEventListener('change', handler);
+    });
 
     const selectedPlayer = $derived.by(async () =>
         (await data.players).find((p: PlayerDisplay) => p.disc === data.disc)
     )
+
+    const index = $derived.by(() => $isDesktop && !data.panel);
 </script>
 
-<div class={data.panel ? "" : "h-full md:grid md:grid-cols-[1fr_2fr] md:grid-rows-[auto_1fr] not-md:pb-[2dvw] gap-4 md:overflow-hidden"}>
-    {#if !data.panel || data.panel === "links"}
+<div class={twMerge("h-full not-md:pb-[2dvw] gap-4 md:overflow-hidden", index ? "md:grid md:grid-cols-[1fr_2fr] md:grid-rows-[auto_1fr]" : "")}>
+    {#if index || activePanel === "links"}
         <Links/>
     {/if}
 
-    {#if !data.panel || data.panel === "symphonists"}
+    {#if index || activePanel === "content"}
+        <Content
+                class="md:row-span-2"
+                player={selectedPlayer}
+                content={data.content}
+        />
+    {/if}
+
+    {#if index || activePanel === "symphonists"}
         {#if data.disc == null}
             <PlayerList
                 class="md:row-start-2"
@@ -29,13 +55,5 @@
                 selectedPlayer={selectedPlayer}
             />
         {/if}
-    {/if}
-
-    {#if !data.panel || data.panel === "content"}
-        <Content
-            class="md:row-span-2"
-            player={selectedPlayer}
-            content={data.content}
-        />
     {/if}
 </div>
